845c533fcb2521721bad8bb50c2ee16952e97e3a
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-21 01:04:06 -0300

    chore: creates aliases

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
+
+const thisPathTo = (...paths) => path.resolve(__dirname, ...paths);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-  }
+  },
+  resolve: {
+    alias: {
+        '@': thisPathTo('src'),
+        '@assets': thisPathTo('src', 'assets'),
+        '@components': thisPathTo('src', 'components'),
+        '@hooks': thisPathTo('src', 'hooks'),
+        '@pages': thisPathTo('src', 'pages'),
+        '@styles': thisPathTo('src', 'styles'),
+    },
+  },
 })
