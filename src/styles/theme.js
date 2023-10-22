e95b354d15c4e22f2e334091f99b21c35173ee1b
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-21 22:24:50 -0300

    style: rename vars

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,22 +1,22 @@
 export const palette = {
-    purple200: '#9546FA',
-    blue200: '#0047ff',
-    light100: '#f4f4f4',
-    light200: '#e7e7e7',
-    light300: '#bbbbbb',
-    gray100: '#888888',
-    dark100: '#242424',
-    dark200: '#141414',
-    dark300: '#0e0e0e',
+    purple200: "#9546FA",
+    blue200: "#0047ff",
+    light100: "#f4f4f4",
+    light200: "#e7e7e7",
+    light300: "#bbbbbb",
+    gray100: "#888888",
+    dark100: "#242424",
+    dark200: "#141414",
+    dark300: "#0e0e0e",
 };
 
 export const paletteRGB = {
-    purple: '149, 70, 250',
-    blue: '0, 71, 255',
+    purple: "149, 70, 250",
+    blue: "0, 71, 255",
 };
 
 export const dark = {
-    current: "dark",
+    title: "dark",
     border: palette.dark100,
     text: palette.light100,
     background: palette.dark300,
@@ -24,7 +24,7 @@ export const dark = {
 };
 
 export const light = {
-    current: "light",
+    title: "light",
     border: palette.light300,
     text: palette.dark300,
     background: palette.light100,
