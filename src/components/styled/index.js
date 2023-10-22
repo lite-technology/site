20a4ea851f8eaabf55b10da08197c7db9e5b4d56
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-22 00:12:20 -0300

    chore: creates legal footer

--- src/components/styled/index.js
+++ src/components/styled/index.js
@@ -1,2 +1,3 @@
 export * from './blue-light-link';
 export * from './floating-header';
+export * from './legal-footer';
--- src/components/styled/legal-footer.js
+++ src/components/styled/legal-footer.js
@@ -1,16 +1,13 @@
 import styled from "styled-components";
 import { getThemeProperty } from "@assets/utils/styled";
 
-export const FloatingHeaderStyled = styled.header`
-position: sticky;
-top: 1rem;
-
+export const LegalFooterStyled = styled.footer`
 display: flex;
 align-items: center;
 justify-content: space-between;
 
 margin: 1rem auto;
-padding: 1.2rem 2.5rem;
+padding: 1rem 2.5rem;
 width: ${getThemeProperty("maxScreenWidth", "layout")};
 max-width: calc(100% - 2rem);
 border-radius: 20rem;
