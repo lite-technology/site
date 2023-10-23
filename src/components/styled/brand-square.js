8e6220000b0ba745f0580a6d97675b4af7dc201f
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-23 19:50:58 -0300

    chore: update base style

--- src/components/styled/brand-square.js
+++ src/components/styled/brand-square.js
@@ -3,7 +3,8 @@ import { getThemeProperty } from "@helpers/utilities";
 
 export const BrandSquareStyled = styled.div`
 position: absolute;
-transform: rotateZ(30deg);
+top: 0;
+left: 0;
 z-index: 1040;
 
 display: flex;
@@ -15,8 +16,8 @@ font-size: 3rem;
 width: var(--size);
 height: var(--size);
 border-radius: 1rem;
-border: 1px solid red;
+border: 1px solid ${getThemeProperty("border", "theme")};
 background-color: ${getThemeProperty("background", "theme")};
 background-image: url(/svgs/square-${getThemeProperty("title", "theme")}.svg);
-box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .15);
+box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .075);
 `;
