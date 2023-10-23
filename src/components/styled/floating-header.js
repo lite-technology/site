eb49267cb9eb82c80816798aff0628577a074609
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-23 00:04:15 -0300

    chore: changes border

--- src/components/styled/floating-header.js
+++ src/components/styled/floating-header.js
@@ -4,6 +4,7 @@ import { getThemeProperty } from "@assets/utils/styled";
 export const FloatingHeaderStyled = styled.header`
 position: sticky;
 top: 1rem;
+z-index: 1050;
 
 display: flex;
 align-items: center;
@@ -14,7 +15,8 @@ margin: 1rem auto;
 padding: 1.2rem 2.5rem;
 width: ${getThemeProperty("maxScreenWidth", "layout")};
 max-width: calc(100% - 2rem);
+border: 1px solid ${getThemeProperty("border", "theme")};
 border-radius: 20rem;
-
 background-color: ${getThemeProperty("cardBackground", "theme")};
+box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .15);
 `;
--- src/components/styled/legal-footer.js
+++ src/components/styled/legal-footer.js
@@ -11,6 +11,7 @@ margin: 1rem auto;
 padding: 1rem 2.5rem;
 width: ${getThemeProperty("maxScreenWidth", "layout")};
 max-width: calc(100% - 2rem);
+border: 1px solid ${getThemeProperty("border", "theme")};
 border-radius: 20rem;
 
 background-color: ${getThemeProperty("cardBackground", "theme")};
