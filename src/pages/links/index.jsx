616b4c9702b39f8ba0848f6a67c44073b3fad940
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-25 19:40:47 -0300

    chore: use linksPage

---
+++ src/pages/links/index.jsx
@@ -0,0 +1,11 @@
+import { HeroSection, NavigationSection, FooterSection } from "@components/links-partials";
+
+export default () => {
+    return (
+        <>
+            <HeroSection />
+            <NavigationSection />
+            <FooterSection />
+        </>
+    );
+};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from "@pages/landingpage";
+import LinksPage from "@pages/links";
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="/links" element={<LinksPage />} />
                 <Route path="*" element={<h1>Error!</h1>} />
             </Routes>
         </BrowserRouter>
